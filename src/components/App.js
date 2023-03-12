35a72e0befc5c0b1f542c25968e3192365608cec
syed ahad <110287582+ahadsyedsh@users.noreply.github.com>
2023-03-12 13:37:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,90 @@
-import React,{useState,useEffect,useRef} from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "../styles/App.css";
 const App = () => {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const [formInput, setFormInput] = useState({
+        email: "",
+        password: "",
+    });
+    const [displayMsg, setDisplayMsg] = useState({
+        emailMsg: "",
+        passwordMsg: "",
+    });
+    const handleInputChange = (e) => {
+        e.persist();
+        // console.log(e.target, e.target.id, e.target.value);
+        if (e.target.id === "inputEmail") {
+            setFormInput((state) => {
+                return {
+                    ...state,
+                    ["email"]: e.target.value,
+                };
+            });
+        } else {
+            setFormInput((state) => {
+                return {
+                    ...state,
+                    ["password"]: e.target.value,
+                };
+            });
+        }
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (formInput.email === "") {
+            emailRef.current.focus();
+            setDisplayMsg({
+                emailMsg: "",
+                passwordMsg: "",
+            });
+        } else if (formInput.password === "") {
+            passwordRef.current.focus();
+            setDisplayMsg({
+                emailMsg: "",
+                passwordMsg: "",
+            });
+        } else {
+            setDisplayMsg({
+                emailMsg: formInput.email,
+                passwordMsg: formInput.password,
+            });
+            setFormInput({
+                email: "",
+                password: "",
+            });
+        }
+    };
+    //code here
 
-//code here 
-
-  return (
-    <div id="main">
-      Email
-      <input id="inputEmail" type="text" value={} ref={} onChange={}/><br/>
-      Password
-      <input id="inputPassword" type="text" value={} ref={} onChange={}/><br/>
-      <button id="submitButton" onClick={}>Submit</button><br/>
-      <p id="emailText">Your Email : {}</p>
-      <p id ="passwordText">Your Password : {}</p>
-      
-    </div>
-  )
-}
-
+    return (
+        <div id="main">
+            Email
+            <input
+                id="inputEmail"
+                type="text"
+                value={formInput.email}
+                ref={emailRef}
+                onChange={handleInputChange}
+            />
+            <br />
+            Password
+            <input
+                id="inputPassword"
+                type="text"
+                value={formInput.password}
+                ref={passwordRef}
+                onChange={handleInputChange}
+            />
+            <br />
+            <button id="submitButton" onClick={handleSubmit}>
+                Submit
+            </button>
+            <br />
+            <p id="emailText">Your Email : {displayMsg.emailMsg}</p>
+            <p id="passwordText">Your Password : {displayMsg.passwordMsg}</p>
+        </div>
+    );
+};
 
 export default App;
